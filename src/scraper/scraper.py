fe85038abed329880f8e0aa513e9baa5f7dd83be
Sonal Jayasinghe <sonaldanindulk@gmail.com>
2025-05-09 08:31:43 +0530

    completed

--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -9,19 +9,15 @@ import time
 from extractor import extract_and_save_coffee_data
 load_dotenv()
 
-# Load environment variables from .env file
 email = os.getenv("EMAIL")
 password = os.getenv("PASSWORD")
 
-# Load Chrome WebDriver
 driver = webdriver.Chrome()
 time.sleep(2)
 
-# Open the login page
 driver.get('https://database.coffeeinstitute.org/login')
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'username')))
 
-#Enter email and password
 username_input = driver.find_element(By.NAME, 'username')
 username_input.send_keys(email)
 password_input = driver.find_element(By.NAME, 'password')
@@ -30,19 +26,15 @@ time.sleep(2)
 login_button = driver.find_element(By.CLASS_NAME, 'submit')
 login_button.click()
 
-#Wait for the page to load
 time.sleep(10)
 
-# find the coffee tab and click it
 coffee_tab = driver.find_element(By.CLASS_NAME, 'coffees')
 coffee_tab.click()
 
-# load the arabica coffee table
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, 'Arabica Coffees'))).click()
 time.sleep(30)
 
 
-# loop through the pages and open each coffee link in a new tab
 while True:
     links = driver.find_elements(By.XPATH, "//table[@data-table='coffees']//a[contains(@href, '/coffee/')]")
     
